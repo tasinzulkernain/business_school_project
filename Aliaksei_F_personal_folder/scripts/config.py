1189c297849f9586be19ac9437441a6467923963
AF_VU <198339040+Outbid9542@users.noreply.github.com>
2025-03-22 11:47:35 +0200

    Clean up: renamed personal folder and files

--- Aliaksei_F_personal_folder/scripts/config.py
+++
@@ -1,7 +0,0 @@
-GAME_MAPPING = {
-    "game1": "Action Mobile Game",
-    "game2": "Puzzle Mobile game",
-    "game3": "Idle Mobile Game",
-}
-
-DEFAULT_GAME_NAME = "Race Mobile Game"  # All other SKUs belong to Race Mobile Game
